cdcb188ff9c1cdc03d5d9366da4092242d1b5aa8
Yazeed-Alwehaibi <yazeed151023@gmail.com>
2025-01-26 22:00:30 +0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Summer Training Platform</h1>
+      <div className="card">
+        <button>log in</button>
+        <button >register</button>
+      </div>
+    </>
+  )
+}
+
+export default App
