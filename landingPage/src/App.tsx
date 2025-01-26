bb30026945fc9d66d0c5e40fb49a9c8066f935e4
Yazeed-Alwehaibi <yazeed151023@gmail.com>
2025-01-26 23:36:04 +0300

    Update App.tsx

--- landingPage/src/App.tsx
+++ landingPage/src/App.tsx
@@ -8,6 +8,8 @@ function App() {
       <div className="card">
         <button>log in</button>
         <button >register</button>
+        <label htmlFor="inputField">Enter your text:</label>
+        <input type="text" id="inputField" name="inputField" placeholder="Type here..."></input>
       </div>
     </>
   )
