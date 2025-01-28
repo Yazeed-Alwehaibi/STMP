b58f51d357f5a2f2903b165290e995d8621f3561
Yazeed-Alwehaibi <yazeed151023@gmail.com>
2025-01-28 13:15:26 +0300

    a

--- landingPage/src/App.tsx
+++ landingPage/src/App.tsx
@@ -5,11 +5,17 @@ function App() {
   return (
     <>
       <h1>Summer Training Platform</h1>
+      <div className='uname'>
+      <label htmlFor="inputField">User name:</label>
+      <input type="text" id="inputField" name="inputField" placeholder="User name"></input>
+      </div>
+      <div className='upass'>
+      <label htmlFor="inputField">Password:</label>
+      <input type="text" id="inputField" name="inputField" placeholder="password"></input>
+      </div>
       <div className="card">
         <button>log in</button>
         <button >register</button>
-        <label htmlFor="inputField">Enter your text:</label>
-        <input type="text" id="inputField" name="inputField" placeholder="Type here..."></input>
       </div>
     </>
   )
