b76edebd5bd46253e7c1d287bc13905c2f04a3af
Yazeed-Alwehaibi <yazeed151023@gmail.com>
2025-02-04 14:04:11 +0300

    landing page

--- landingPage/src/App.tsx
+++ landingPage/src/App.tsx
@@ -1,19 +1,44 @@
-import './App.css'
+import './App.css';
+import { useState } from 'react';
 
-function App() {
+
+const TextInput = () => {
+ 
+    const [text, setText] = useState<string>('')
 
   return (
     <>
       <h1>Summer Training Platform</h1>
+      <br />
+      <br />
       <div className="card">
-        <button>log in</button>
-        <button>register</button>
-        <button> test</button>
-        <label htmlFor="inputField">Enter your text:</label>
-        <input type="text" id="inputField" name="inputField" placeholder="Type here..."></input>
-      </div>
+        <input type="text" value={text} onChange={(e:React.ChangeEvent<HTMLInputElement>) =>
+          setText(e.target.value)
+        } 
+        placeholder="           USERNAME"
+        className="w-full p-2 rounded-full border border-gray bg-white text-black  focus:outline-none focus:ring-1 focus:ring-white-500" 
+        />
+        <br />
+        <br />
+      <input type="text" value={text} onChange={(e:React.ChangeEvent<HTMLInputElement>) =>
+          setText(e.target.value)
+        } 
+        placeholder="           PASSWORD"
+        className="w-full p-2 rounded-full border border-gray bg-white text-black  focus:outline-none focus:ring-1 focus:ring-white-500" 
+        />
+        
+        <br />
+        <br />
+        <button className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition">
+          log in
+        </button>
+        <button className="px-4 py-2 bg-blue-50 text-white rounded-md hover:bg-blue-600 transition">
+          register
+        </button>
+        </div>
     </>
   )
 }
 
-export default App
+export default TextInput;
+
