03d0c3df7c27f47bfde86f671949cb23392cdb59
Yazeed-Alwehaibi <yazeed151023@gmail.com>
2025-02-08 16:45:43 +0300

    a

--- landingPage/src/pages/landingPage.tsx
+++ landingPage/src/pages/landingPage.tsx
@@ -6,7 +6,8 @@ import { useNavigate } from 'react-router-dom';
 
 const TextInput = () => {
  
-    const [text, setText] = useState<string>('')
+    const [username, setUsername] = useState<string>('')
+    const [password, setPassword] = useState<string>('')
     const navigate = useNavigate();
     const handleregisterclick = () => {
         
@@ -20,16 +21,16 @@ const TextInput = () => {
       <br />
       <br />
       <div className="card">
-        <input type="text" value={text} onChange={(e:React.ChangeEvent<HTMLInputElement>) =>
-          setText(e.target.value)
+        <input type="text" value={username} onChange={(e:React.ChangeEvent<HTMLInputElement>) =>
+          setUsername(e.target.value)
         } 
         placeholder="           USERNAME"
         className="w-full p-2 rounded-full border border-gray bg-white text-black  focus:outline-none focus:ring-1 focus:ring-white-500" 
         />
         <br />
         <br />
-      <input type="text" value={text} onChange={(e:React.ChangeEvent<HTMLInputElement>) =>
-          setText(e.target.value)
+      <input type="password" value={password} onChange={(e:React.ChangeEvent<HTMLInputElement>) =>
+          setPassword(e.target.value)
         } 
         placeholder="           PASSWORD"
         className="w-full p-2 rounded-full border border-gray bg-white text-black  focus:outline-none focus:ring-1 focus:ring-white-500" 
